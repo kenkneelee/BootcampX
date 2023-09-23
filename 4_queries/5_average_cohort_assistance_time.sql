c35a819e7e1670290b0ba90bd6b9b6946d0fcab4
Kenny Lee <kenny.lee927@gmail.com>
2023-09-23 01:58:26 -0400

    complete queries 1 through 5

---
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name, AVG(completed_at - started_at) as average_assistance_time
+FROM cohorts
+JOIN students ON cohort_id = cohorts.id
+JOIN assistance_requests ON students.id = student_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time
